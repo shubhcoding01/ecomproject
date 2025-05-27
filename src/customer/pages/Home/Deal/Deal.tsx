bf468158d00b59c95a45e9e70c5ff7501a7218fb
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-27 22:47:52 +0530

    Update DealCard component to use new props structure

--- src/customer/pages/Home/Deal/Deal.tsx
+++ src/customer/pages/Home/Deal/Deal.tsx
@@ -4,7 +4,7 @@ import DealCard from './DealCard'
 const Deal = () => {
   return (
     <div className='py-5 lg:px-20'>
-        <div>
+        <div className='flex items-center justify-between'>
           {[1,1,1,1,1,1].map((item)=><DealCard/>)}
         </div>
     </div>
