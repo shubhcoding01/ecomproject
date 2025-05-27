8dfa3ab147e100f71e52a4b9ccc63ce3d55a1686
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-27 22:26:45 +0530

    Update DealCard component to use new props structure

--- src/customer/pages/Home/Deal/DealCard.tsx
+++ src/customer/pages/Home/Deal/DealCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DealCard = () => {
   return (
-    <div>
+    <div className='w-[30rem] cursor-pointer'>
       <img src="https://m.media-amazon.com/images/I/61-vRq2ulOL._AC_UY327_FMwebp_QL65_.jpg" alt="" />
 
       <div>
