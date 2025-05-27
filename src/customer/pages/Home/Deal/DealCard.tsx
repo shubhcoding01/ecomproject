6e993f8792462bd67f2da6d16430c8684fd915da
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-27 22:34:07 +0530

    Update DealCard component to use new props structure

--- src/customer/pages/Home/Deal/DealCard.tsx
+++ src/customer/pages/Home/Deal/DealCard.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 const DealCard = () => {
   return (
-    <div className='w-[14rem] cursor-pointer'>
+    <div className='w-[12rem] cursor-pointer'>
       <img className='border-x-[7px] border-t-[7px] border-pink-500 w-full
       h-[12rem] object-cover object-top' src="https://m.media-amazon.com/images/I/61-vRq2ulOL._AC_UY327_FMwebp_QL65_.jpg" alt="" />
 
-      <div>
+      <div className='border-4 border-black bg-black text-white
+      p-2 text-center'>
         <p className='text-lg font-semibold'>Smart Watch</p>
         <p className='text-2xl font-bold'>20% Off</p>
         <p className='text-balance text-lg'>Shop Now</p>
