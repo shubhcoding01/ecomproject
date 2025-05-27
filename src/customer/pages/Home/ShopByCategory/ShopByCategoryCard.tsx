28ef9629d03f76fa81be73bbec02fa83b7bb75a4
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-28 00:00:09 +0530

    Updated Home component to include a new feature

--- src/customer/pages/Home/ShopByCategory/ShopByCategoryCard.tsx
+++ src/customer/pages/Home/ShopByCategory/ShopByCategoryCard.tsx
@@ -4,7 +4,7 @@ const ShopByCategoryCard = () => {
   return (
     <div className='flex gap-3 flex-col justify-center items-center group cursor-pointer'>
 
-        <div className='w-[150px] h-[150px] relative overflow-hidden rounded-full'>
+        <div className='w-[150px] h-[150px] relative overflow-hidden rounded-full bg-primary-color'>
             <img className='group-hover:scale-95 transition-transform 
             transition-duration-700 w-full h-full object-cover object-top' 
             src="https://m.media-amazon.com/images/I/619IDSfXDoL._AC_UL480_FMwebp_QL65_.jpg" alt="" />
