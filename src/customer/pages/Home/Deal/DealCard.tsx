5726f33980d02cca8168b992d74c909f25941dc0
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-27 22:21:31 +0530

    Updated Home component to include a new feature

--- src/customer/pages/Home/Deal/DealCard.tsx
+++ src/customer/pages/Home/Deal/DealCard.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const DealCard = () => {
   return (
-    <div>DealCard</div>
+    <div>
+      <img src="https://m.media-amazon.com/images/I/61-vRq2ulOL._AC_UY327_FMwebp_QL65_.jpg" alt="" />
+
+      <div>
+        <p></p>
+      </div>
+    </div>
   )
 }
 
