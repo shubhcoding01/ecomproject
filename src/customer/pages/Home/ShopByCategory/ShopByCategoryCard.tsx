8e27cbdec2c66f3dbb0d887d5c62946b33675b4b
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-27 23:55:58 +0530

    Update DealCard component to use new props structure

--- src/customer/pages/Home/ShopByCategory/ShopByCategoryCard.tsx
+++ src/customer/pages/Home/ShopByCategory/ShopByCategoryCard.tsx
@@ -5,7 +5,9 @@ const ShopByCategoryCard = () => {
     <div className='flex gap-3 flex-col justify-center items-center group cursor-pointer'>
 
         <div>
-            <img src="https://m.media-amazon.com/images/I/619IDSfXDoL._AC_UL480_FMwebp_QL65_.jpg" alt="" />
+            <img className='group-hover:scale-95 transition-transform 
+            transition-duration-700 w-full h-full object-cover object-top' 
+            src="https://m.media-amazon.com/images/I/619IDSfXDoL._AC_UL480_FMwebp_QL65_.jpg" alt="" />
         </div>
         <h1>Kitchen</h1>
     </div>
