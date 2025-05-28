741a8f0dbf864e029929dc4886091f1b36d799e6
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-28 14:43:06 +0530

    Update Home component to include new features

---
+++ src/data/category/level two/electronicsLevelTwo.ts
@@ -0,0 +1,37 @@
+export const electronicsLevelTwo = [
+  {
+    name: "Mobiles",
+    categoryId: "electronics_mobiles",
+    parentCategoryId: "electronics",
+    level: 2,
+    levelThreeCategory: []
+  },
+  {
+    name: "Laptops",
+    categoryId: "electronics_laptops",
+    parentCategoryId: "electronics",
+    level: 2,
+    levelThreeCategory: []
+  },
+  {
+    name: "Televisions",
+    categoryId: "electronics_televisions",
+    parentCategoryId: "electronics",
+    level: 2,
+    levelThreeCategory: []
+  },
+  {
+    name: "Audio",
+    categoryId: "electronics_audio",
+    parentCategoryId: "electronics",
+    level: 2,
+    levelThreeCategory: []
+  },
+  {
+    name: "Cameras",
+    categoryId: "electronics_cameras",
+    parentCategoryId: "electronics",
+    level: 2,
+    levelThreeCategory: []
+  }
+];
