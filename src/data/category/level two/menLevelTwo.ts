2a96127549b632ccee5672a62a4f1fc04ee56c33
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-28 14:40:36 +0530

    Update Home component to include new features

---
+++ src/data/category/level two/menLevelTwo.ts
@@ -0,0 +1,37 @@
+export const menLevelTwo = [
+  {
+    name: "Topwear",
+    categoryId: "men_topwear",
+    parentCategoryId: "men",
+    level: 2,
+    levelThreeCategory: []
+  },
+  {
+    name: "Bottomwear",
+    categoryId: "men_bottomwear",
+    parentCategoryId: "men",
+    level: 2,
+    levelThreeCategory: []
+  },
+  {
+    name: "Footwear",
+    categoryId: "men_footwear",
+    parentCategoryId: "men",
+    level: 2,
+    levelThreeCategory: []
+  },
+  {
+    name: "Accessories",
+    categoryId: "men_accessories",
+    parentCategoryId: "men",
+    level: 2,
+    levelThreeCategory: []
+  },
+  {
+    name: "Sportswear",
+    categoryId: "men_sportswear",
+    parentCategoryId: "men",
+    level: 2,
+    levelThreeCategory: []
+  }
+];
