21e759a487fca2aeac0c28afff7be22bcf89a70e
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-28 12:45:30 +0530

    Update ShopByCategory.css

--- src/customer/pages/Home/Home.tsx
+++ src/customer/pages/Home/Home.tsx
@@ -19,14 +19,14 @@ const Home = () => {
               <Deal/>
             </div>
 
-            <section className='pt-20'>
+            <section className='pt-20 '>
               <h1 className='text-lg lg:text-4xl font-bold text-center
                text-primary-color pb-5 lg:pb-20'>Shop By Category</h1>
               <ShopByCategory/>
             </section>
 
-            <section className='pt-20 lg:px-20 relative h-[200px] lg:h-[450px] object-cover'>
-              <img className='w-full h-full' 
+            <section className=' pt-20 lg:px-20 relative h-[200px] lg:h-[450px] object-cover'>
+              <img className='w-full h-full absolute top-0 left-0 object-cover object-center rounded-lg' 
               src="https://sdmntprnorthcentralus.oaiusercontent.com/files/00000000-d5cc-622f-9c54-470531e98811/raw?se=2025-05-28T07%3A51%3A43Z&sp=r&sv=2024-08-04&sr=b&scid=92fe22e1-b954-5741-bccd-57d95ffb7936&skoid=bbd22fc4-f881-4ea4-b2f3-c12033cf6a8b&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-05-27T21%3A21%3A27Z&ske=2025-05-28T21%3A21%3A27Z&sks=b&skv=2024-08-04&sig=r/z7QbxcHOrnXC3kJRRdAvHcy0rQCUPjMkWv2XIoVTA%3D" alt="" />
               <div className='absolute top-1/2 left-4 lg:left-[15rem] 
               transform translate-y-1/2 font-semibold lg-text-4xl space-y-3'>
