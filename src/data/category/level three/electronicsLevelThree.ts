626183c12ee1ceaa2d353251f63b3a3b6060870f
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-28 14:47:30 +0530

    Update Home component to include new features

---
+++ src/data/category/level three/electronicsLevelThree.ts
@@ -0,0 +1,32 @@
+export const electronicsLevelThree = [
+  {
+    name: "Smartphones",
+    categoryId: "electronics_mobiles_smartphones",
+    parentCategoryId: "electronics_mobiles",
+    level: 3
+  },
+  {
+    name: "Feature Phones",
+    categoryId: "electronics_mobiles_featurephones",
+    parentCategoryId: "electronics_mobiles",
+    level: 3
+  },
+  {
+    name: "Gaming Laptops",
+    categoryId: "electronics_laptops_gaming",
+    parentCategoryId: "electronics_laptops",
+    level: 3
+  },
+  {
+    name: "Smart TVs",
+    categoryId: "electronics_televisions_smart",
+    parentCategoryId: "electronics_televisions",
+    level: 3
+  },
+  {
+    name: "Bluetooth Speakers",
+    categoryId: "electronics_audio_bluetooth_speakers",
+    parentCategoryId: "electronics_audio",
+    level: 3
+  }
+];
