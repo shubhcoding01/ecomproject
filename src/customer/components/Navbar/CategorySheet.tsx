08dcaf9e4bd0d9068e855acab7dea3fa8b7ede5d
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-28 14:54:30 +0530

    Update CategorySheet

--- src/customer/components/Navbar/CategorySheet.tsx
+++ src/customer/components/Navbar/CategorySheet.tsx
@@ -1,4 +1,19 @@
 import React from 'react'
+import { menLevelTwo } from '../../../data/category/level two/menLevelTwo'
+import { womenLevelTwo } from '../../../data/category/level two/womenLevelTwo'
+import { electronicsLevelTwo } from '../../../data/category/level two/electronicsLevelTwo'
+import { furnitureLevelTwo } from '../../../data/category/level two/furnitureLevelTwo'
+
+const categoryTwo = {
+    men:menLevelTwo,
+    women:womenLevelTwo,
+    electronics:electronicsLevelTwo,
+    furniture:furnitureLevelTwo
+}
+
+const categoryThree = {
+
+}
 
 const CategorySheet = () => {
   return (
