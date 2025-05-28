407185305ffe5b2152ffbbdd6e07e7fc7ddb2d9d
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-28 11:34:58 +0530

    Update ShopByCategory.css

--- src/customer/pages/Home/ShopByCategory/ShopByCategoryCard.tsx
+++ src/customer/pages/Home/ShopByCategory/ShopByCategoryCard.tsx
@@ -5,7 +5,7 @@ const ShopByCategoryCard = () => {
     <div className='flex gap-3 flex-col justify-center items-center group cursor-pointer'>
 
         <div className='custome-border w-[150px] h-[150px] lg:w-[249px] lg:h-[249px] relative overflow-hidden rounded-full bg-primary-color'>
-            <img className='group-hover:scale-95 transition-transform 
+            <img className='rounded-full group-hover:scale-95 transition-transform 
             transition-duration-700 w-full h-full object-cover object-top' 
             src="https://m.media-amazon.com/images/I/619IDSfXDoL._AC_UL480_FMwebp_QL65_.jpg" alt="" />
         </div>
