b8635c07ba20b107ef906e6595273ee4c45a3cd9
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-28 12:24:24 +0530

    Update ShopByCategory.css

--- src/customer/pages/Home/Home.tsx
+++ src/customer/pages/Home/Home.tsx
@@ -26,7 +26,14 @@ const Home = () => {
             </section>
 
             <section className='pt-20 lg:px-20 relative h-[200px] lg:h-[450px] object-cover'>
-              <img className='w-full h-full' src="https://t4.ftcdn.net/jpg/01/89/49/37/360_F_189493703_22JMn19BCJuTJAkb9tfhOWpmGHCKungn.jpg" alt="" />
+              <img className='w-full h-full' 
+              src="https://sdmntprnorthcentralus.oaiusercontent.com/files/00000000-d5cc-622f-9c54-470531e98811/raw?se=2025-05-28T07%3A51%3A43Z&sp=r&sv=2024-08-04&sr=b&scid=92fe22e1-b954-5741-bccd-57d95ffb7936&skoid=bbd22fc4-f881-4ea4-b2f3-c12033cf6a8b&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-05-27T21%3A21%3A27Z&ske=2025-05-28T21%3A21%3A27Z&sks=b&skv=2024-08-04&sig=r/z7QbxcHOrnXC3kJRRdAvHcy0rQCUPjMkWv2XIoVTA%3D" alt="" />
+              <div className='absolute top-1/2 left-4 lg:left-[15rem] 
+              transform translate-y-1/2 font-semibold lg-text-4xl space-y-3'>
+                <h1>Sell Your Product</h1>
+                <p className='text-lg md:text-2xl'>With</p>
+                <p className='logo'>Desi Bazaar</p>
+              </div>
             </section>
             
         </div>
