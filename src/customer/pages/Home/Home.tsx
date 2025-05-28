937dbc8fad3e2e697cd4cce1fcfe08891b4eb8a6
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-28 12:03:20 +0530

    Update ShopByCategory.css

--- src/customer/pages/Home/Home.tsx
+++ src/customer/pages/Home/Home.tsx
@@ -19,11 +19,15 @@ const Home = () => {
               <Deal/>
             </div>
 
-            <div className='pt-20'>
+            <section className='pt-20'>
               <h1 className='text-lg lg:text-4xl font-bold text-center
                text-primary-color pb-5 lg:pb-20'>Shop By Category</h1>
               <ShopByCategory/>
-            </div>
+            </section>
+
+            <section className='pt-20 lg:px-20 relative h-[200px] lg:h-[450px] object-cover'>
+              <img className='w-full h-full' src="https://t4.ftcdn.net/jpg/01/89/49/37/360_F_189493703_22JMn19BCJuTJAkb9tfhOWpmGHCKungn.jpg" alt="" />
+            </section>
             
         </div>
     </>
