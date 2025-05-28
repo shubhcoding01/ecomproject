dccd569a613c594f8b669170fa5863ecdb1d5ab1
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-28 14:41:32 +0530

    Update Home component to include new features

---
+++ src/data/category/level two/womenLevelTwo.ts
@@ -0,0 +1,37 @@
+export const womenLevelTwo = [
+  {
+    name: "Indian & Fusion Wear",
+    categoryId: "women_indian_and_fusion_wear",
+    parentCategoryId: "women",
+    level: 2,
+    levelThreeCategory: []
+  },
+  {
+    name: "Western Wear",
+    categoryId: "women_western_wear",
+    parentCategoryId: "women",
+    level: 2,
+    levelThreeCategory: []
+  },
+  {
+    name: "Footwear",
+    categoryId: "women_footwear",
+    parentCategoryId: "women",
+    level: 2,
+    levelThreeCategory: []
+  },
+  {
+    name: "Lingerie & Sleepwear",
+    categoryId: "women_lingerie",
+    parentCategoryId: "women",
+    level: 2,
+    levelThreeCategory: []
+  },
+  {
+    name: "Accessories",
+    categoryId: "women_accessories",
+    parentCategoryId: "women",
+    level: 2,
+    levelThreeCategory: []
+  }
+];
