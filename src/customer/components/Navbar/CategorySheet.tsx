504ed638a4b814a39af65dba8e13eb9abab08603
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-28 14:55:23 +0530

    Update Home component to include new features

--- src/customer/components/Navbar/CategorySheet.tsx
+++ src/customer/components/Navbar/CategorySheet.tsx
@@ -3,6 +3,7 @@ import { menLevelTwo } from '../../../data/category/level two/menLevelTwo'
 import { womenLevelTwo } from '../../../data/category/level two/womenLevelTwo'
 import { electronicsLevelTwo } from '../../../data/category/level two/electronicsLevelTwo'
 import { furnitureLevelTwo } from '../../../data/category/level two/furnitureLevelTwo'
+import { menLevelThree } from '../../../data/category/level three/menLevelThree'
 
 const categoryTwo = {
     men:menLevelTwo,
@@ -12,7 +13,10 @@ const categoryTwo = {
 }
 
 const categoryThree = {
-
+    men:menLevelThree,
+    women:womenLevelTwo,
+    electronics:electronicsLevelTwo,
+    furniture:furnitureLevelTwo
 }
 
 const CategorySheet = () => {
