8dd2c085140d3244105781e4271e9a6a1bd01cfb
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-29 10:21:12 +0530

    Update Navbar component to use new styles

--- src/customer/pages/Product/Product.tsx
+++ src/customer/pages/Product/Product.tsx
@@ -7,7 +7,10 @@ const Product = () => {
             <h1 className="text-3xl text-center font-bold text-gray-600 pb-5 px-9
             uppercase space-x-2">Women Saree</h1>
         </div>
-        <div className=''></div>
+        <div className='lg:flex'>
+            <section className="filter_section"></section>
+            <section className="product_section"></section>
+        </div>
     </div>
   )
 }
