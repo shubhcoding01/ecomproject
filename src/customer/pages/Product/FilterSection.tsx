f7f7e084a0573524e3649bb5cfe49ecf483adeff
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-29 16:19:38 +0530

    Update FilterSection

--- src/customer/pages/Product/FilterSection.tsx
+++ src/customer/pages/Product/FilterSection.tsx
@@ -1,8 +1,18 @@
+import { Button } from '@mui/material'
 import React from 'react'
 
 const FilterSection = () => {
   return (
-    <div>FilterSection</div>
+    <div className='z-50 space-y-5 bg-white'>
+      <div className='flex items-start justify-between h-[40px] px-9 lg:border-r'>
+        <p className='text-lg font-semibold'>
+          Filter
+          </p>
+          <Button size='small' className='text-blue-500 cursor-pointer font-semibold'>
+            Clear all
+          </Button>
+      </div>
+    </div>
   )
 }
 
