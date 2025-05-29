7ec32f3eb4f1969ccc3776ad68a4c3fba39e89a5
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-29 11:16:39 +0530

    Update Product component to handle product details and reviews

--- src/customer/pages/Product/ProductCard.tsx
+++ src/customer/pages/Product/ProductCard.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
 const ProductCard = () => {
+    const images = [
+        "https://m.media-amazon.com/images/I/61HS4sTDnPL._SY741_.jpg",
+        "https://m.media-amazon.com/images/I/61U0B7tRy+L._SY741_.jpg"
+    ];
   return (
-    <div>ProductCard</div>
+    <>
+        <div className="group px-4 relative">
+            <div className='card'>
+
+                {images.map((item) => <img className='card-media object-top'
+                    src={item}
+                    alt=""/>)}
+            </div>
+        </div>
+    </>
   )
 }
 
