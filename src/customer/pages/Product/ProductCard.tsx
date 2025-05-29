e1b71948a0baea0a68db2cbe0841bf38ec57ad64
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-29 11:42:15 +0530

    Update ProductCard styles

--- src/customer/pages/Product/ProductCard.tsx
+++ src/customer/pages/Product/ProductCard.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import './ProductCard.css'; // Assuming you have a CSS file for styling
 
 const ProductCard = () => {
     const images = [
         "https://m.media-amazon.com/images/I/61HS4sTDnPL._SY741_.jpg",
+        "https://m.media-amazon.com/images/I/61HS4sTDnPL._SY741_.jpg",
+        "https://m.media-amazon.com/images/I/51P1YW2yAGL._SY741_.jpg",
         "https://m.media-amazon.com/images/I/61U0B7tRy+L._SY741_.jpg"
     ];
   return (
@@ -10,10 +13,11 @@ const ProductCard = () => {
         <div className="group px-4 relative">
             <div className='card'>
 
-                {images.map((item) => <img className='card-media object-top'
+                {images.map((item) => <img 
+                className='card-media object-top'
                     src={item}
                     alt=""/>)}
-                    
+
             </div>
         </div>
     </>
