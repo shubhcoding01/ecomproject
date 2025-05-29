0f2cb7ed526b2b204da1e4b2af725ea392534524
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-29 11:57:56 +0530

    Update ProductCard styles

--- src/customer/pages/Product/ProductCard.tsx
+++ src/customer/pages/Product/ProductCard.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProductCard.css'; // Assuming you have a CSS file for styling
 
-const ProductCard = () => {
-    const images = [
+const images = [
         "https://m.media-amazon.com/images/I/61HS4sTDnPL._SY741_.jpg",
         "https://m.media-amazon.com/images/I/61XSVitox-L._SY741_.jpg",
         "https://m.media-amazon.com/images/I/51P1YW2yAGL._SY741_.jpg",
         "https://m.media-amazon.com/images/I/61OgwPCLMsL._SY741_.jpg",
         "https://m.media-amazon.com/images/I/61U0B7tRy+L._SY741_.jpg"
     ];
+
+const ProductCard = () => {
+    const [currentImage, setCurrentImage] = useState(0);
+    const [isHovered, setIsHovered] = useState(false);
+
   return (
     <>
         <div className="group px-4 relative">
