b5974b58d24d1fb9a9d9555bad000b00cbb02df7
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-29 11:49:10 +0530

    Update ProductCard styles

--- src/customer/pages/Product/ProductCard.tsx
+++ src/customer/pages/Product/ProductCard.tsx
@@ -4,7 +4,7 @@ import './ProductCard.css'; // Assuming you have a CSS file for styling
 const ProductCard = () => {
     const images = [
         "https://m.media-amazon.com/images/I/61HS4sTDnPL._SY741_.jpg",
-        "https://m.media-amazon.com/images/I/61HS4sTDnPL._SY741_.jpg",
+        "https://m.media-amazon.com/images/I/61XSVitox-L._SY741_.jpg",
         "https://m.media-amazon.com/images/I/51P1YW2yAGL._SY741_.jpg",
         "https://m.media-amazon.com/images/I/61OgwPCLMsL._SY741_.jpg",
         "https://m.media-amazon.com/images/I/61U0B7tRy+L._SY741_.jpg"
@@ -14,10 +14,12 @@ const ProductCard = () => {
         <div className="group px-4 relative">
             <div className='card'>
 
-                {images.map((item) => <img 
+                {images.map((item,index) => <img 
                 className='card-media object-top'
                     src={item}
-                    alt=""/>)}
+                    alt=""
+                    style={{transform:`translateX(${(index-1)*100}%)`}}
+                    />)}
 
             </div>
         </div>
