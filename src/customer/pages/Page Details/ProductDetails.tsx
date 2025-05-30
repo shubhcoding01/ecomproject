870ab94eec46603985f65d49cac19c76deb76001
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-30 13:54:34 +0530

    Update ProductDetails

--- src/customer/pages/Page Details/ProductDetails.tsx
+++ src/customer/pages/Page Details/ProductDetails.tsx
@@ -7,7 +7,7 @@ const ProductDetails = () => {
         <section className='flex flex-col lg:flex-row gap-5'>
           <div className='w-full lg:w-[15%] flex flex-wrap lg:flex-col gap-3'>
               {[1,1,1,1].map((item)=><img 
-              
+              className='lg:w-full w-[50px] cursor-pointer rounded-md'
               src='https://m.media-amazon.com/images/I/61HS4sTDnPL._SY741_.jpg' alt=''/>)}
           </div>
           <div className='w-full lg:w-[85%]'>
