a881002414742cccce2833bbb79cf9711a288d80
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-30 09:50:41 +0530

    Update the FilterSection

--- src/customer/pages/Product/FilterSection.tsx
+++ src/customer/pages/Product/FilterSection.tsx
@@ -1,35 +1,48 @@
-import { Button, Divider, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material'
-import React from 'react'
+import {
+  Button,
+  Divider,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
+import React from "react";
 
 const FilterSection = () => {
   return (
-    <div className='z-50 space-y-5 bg-white'>
-      <div className='flex items-start justify-between h-[40px] px-9 lg:border-r'>
-        <p className='text-lg font-semibold'>
-          Filter
-          </p>
-          <Button size='small' className='text-blue-500 cursor-pointer font-semibold'>
-            Clear all
-          </Button>
+    <div className="z-50 space-y-5 bg-white">
+      <div className="flex items-start justify-between h-[40px] px-9 lg:border-r">
+        <p className="text-lg font-semibold">Filter</p>
+        <Button
+          size="small"
+          className="text-blue-500 cursor-pointer font-semibold"
+        >
+          Clear all
+        </Button>
       </div>
       <Divider />
 
       <section>
         <FormControl>
-  <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
-  <RadioGroup
-    aria-labelledby="demo-radio-buttons-group-label"
-    defaultValue="female"
-    name="radio-buttons-group"
-  >
-    <FormControlLabel value="female" control={<Radio />} label="Female" />
-    <FormControlLabel value="male" control={<Radio />} label="Male" />
-    <FormControlLabel value="other" control={<Radio />} label="Other" />
-  </RadioGroup>
-</FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="female"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel
+              value="female"
+              control={<Radio />}
+              label="Female"
+            />
+            <FormControlLabel value="male" control={<Radio />} label="Male" />
+            <FormControlLabel value="other" control={<Radio />} label="Other" />
+          </RadioGroup>
+        </FormControl>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default FilterSection
+export default FilterSection;
