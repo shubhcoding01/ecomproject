dcce4ff95a7392918a0f7b3536189b6dc3516996
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-30 13:52:43 +0530

    Update ProductDetails

--- src/customer/pages/Page Details/ProductDetails.tsx
+++ src/customer/pages/Page Details/ProductDetails.tsx
@@ -6,7 +6,9 @@ const ProductDetails = () => {
       <div className='grid grid-cols-1 lg:grid-cols-2 gap-10'>
         <section className='flex flex-col lg:flex-row gap-5'>
           <div className='w-full lg:w-[15%] flex flex-wrap lg:flex-col gap-3'>
-
+              {[1,1,1,1].map((item)=><img 
+              
+              src='https://m.media-amazon.com/images/I/61HS4sTDnPL._SY741_.jpg' alt=''/>)}
           </div>
           <div className='w-full lg:w-[85%]'>
               <img 
