3a34f3e6d862aba6906c2da24cadccbf37338b38
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-31 00:33:04 +0530

    Implement SimilarProduct and SimilarProductCard components with grid layout and product details

--- src/customer/pages/Page Details/SimilarProductCard.tsx
+++ src/customer/pages/Page Details/SimilarProductCard.tsx
@@ -2,7 +2,38 @@ import React from 'react'
 
 const SimilarProductCard = () => {
   return (
-    <div>SimilarProductCard</div>
+    <div>
+        <div className="group px-4 relative">
+            <div className='card'
+                
+
+                 <img 
+                className='card-media object-top'
+                    src={"https://m.media-amazon.com/images/I/81j27szi3cL._SY741_.jpg"}
+                    alt=""/>
+
+                   
+            </div>
+            <div className='details pt-3 space-y-1 group-hover-effect rounded-md'>
+                    <div className='name'>
+                        <h1>Nike</h1>
+                        <p>Stylish Shirt</p>
+                    </div>
+                    <div className='price flex items-center gap-3'>
+                        <span className='font-sans text-gray-800'>
+                            ₹ 400
+                            </span>
+                            <span className='thin-line-through text-gray-400'>
+                            ₹ 999
+                            </span>
+                            <span className='text-primary-color font-semibold'>
+                            60% off
+
+                            </span>
+                    </div>
+            </div>
+        </div>
+    
   )
 }
 
