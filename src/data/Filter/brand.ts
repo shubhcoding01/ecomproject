bc783c5028de0dfd0df6cdc11e7ab062d04e0aca
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-30 11:13:51 +0530

    Update brand

---
+++ src/data/Filter/brand.ts
@@ -0,0 +1,17 @@
+export const brand = [
+  { name: "Nike", value: "nike" },
+  { name: "Adidas", value: "adidas" },
+  { name: "Puma", value: "puma" },
+  { name: "Reebok", value: "reebok" },
+  { name: "Levi's", value: "levis" },
+  { name: "H&M", value: "hm" },
+  { name: "Zara", value: "zara" },
+  { name: "Under Armour", value: "under_armour" },
+  { name: "New Balance", value: "new_balance" },
+  { name: "ASICS", value: "asics" },
+  { name: "Tommy Hilfiger", value: "tommy_hilfiger" },
+  { name: "Gucci", value: "gucci" },
+  { name: "Prada", value: "prada" },
+  { name: "Louis Vuitton", value: "louis_vuitton" },
+  { name: "U.S. Polo Assn.", value: "us_polo" }
+];
