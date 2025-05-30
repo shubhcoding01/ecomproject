77c52c27724e3bb94f1cdeadad77cd37e71bdb78
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-30 13:46:40 +0530

    Update ProductDetails

--- src/customer/pages/Page Details/ProductDetails.tsx
+++ src/customer/pages/Page Details/ProductDetails.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 const ProductDetails = () => {
   return (
     <div className='px-5 lg:px-20 pt-20'>
-      <div className=''>
-        <section>
-          <div>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-10'>
+        <section className='flex flex-col lg:flex-row gap-5'>
+          <div className='w-full lg:w-[15%] flex flex-wrap lg:flex-col gap-3'>
 
           </div>
           <div>
-            
+
           </div>
         </section>
       </div>
