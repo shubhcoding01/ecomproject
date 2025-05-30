3cf4fa5276bfd0e9a54ff2e009888dcbea6eb95e
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-30 13:43:48 +0530

    Update ProductDetails

--- src/customer/pages/Page Details/ProductDetails.tsx
+++ src/customer/pages/Page Details/ProductDetails.tsx
@@ -3,7 +3,16 @@ import React from 'react'
 const ProductDetails = () => {
   return (
     <div className='px-5 lg:px-20 pt-20'>
+      <div className=''>
+        <section>
+          <div>
 
+          </div>
+          <div>
+            
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
