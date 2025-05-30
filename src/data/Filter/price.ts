dfb431305facd2099c147ff1cd3bfd16b9a467ce
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-30 11:11:29 +0530

    Update price

---
+++ src/data/Filter/price.ts
@@ -0,0 +1,14 @@
+export const price = [
+  { name: "Under ₹500", min: 0, max: 500, value: "500" },
+  { name: "₹500 - ₹999", min: 500, max: 999, value: "999" },
+  { name: "₹1000 - ₹1999", min: 1000, max: 1999, value: "1999" },
+  { name: "₹2000 - ₹2999", min: 2000, max: 2999, value: "2999" },
+  { name: "₹3000 - ₹3999", min: 3000, max: 3999, value: "3999" },
+  { name: "₹4000 - ₹4999", min: 4000, max: 4999, value: "4999" },
+  { name: "₹5000 - ₹5999", min: 5000, max: 5999, value: "5999" },
+  { name: "₹6000 - ₹6999", min: 6000, max: 6999, value: "6999" },
+  { name: "₹7000 - ₹7999", min: 7000, max: 7999, value: "7999" },
+  { name: "₹8000 - ₹8999", min: 8000, max: 8999, value: "8999" },
+  { name: "₹9000 - ₹9999", min: 9000, max: 9999, value: "9999" },
+  { name: "₹10000 and above", min: 10000, max: Infinity, value: "10000" }
+];
