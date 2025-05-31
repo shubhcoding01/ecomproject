1afe14e73e97b2ae72751cfbd5e99f07c2db8d8b
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-31 14:17:16 +0530

    Refactor CartItem component for improved readability and consistency

--- src/customer/pages/Cart/CartItem.tsx
+++ src/customer/pages/Cart/CartItem.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const CartItem = () => {
   return (
-    <div className='border rounded-md relative'>
-        <div className='p-5 flex gap-3'>
-          <img className='w-[90px] rounded-md' src="https://m.media-amazon.com/images/I/61HS4sTDnPL._AC_AA152_.jpg" alt="" />
-        </div>
+    <div className="border rounded-md relative">
+      <div className="p-5 flex gap-3">
+        <img
+          className="w-[90px] rounded-md"
+          src="https://m.media-amazon.com/images/I/61HS4sTDnPL._AC_AA152_.jpg"
+          alt=""
+        />
+      </div>
+      <div className="space-y-2 ">
+        <h1 className="font-semibold text-lg">
+          Men's Irregular Geometric Pattern
+        </h1>
+        <p className="text-gray-600 font-medium text-sm">
+          Stay stylish and comfortable with this geometric-patterned polo
+          T-shirt
+        </p>
+        <p className="text-gray-400 text-xs">
+          <strong>Sold by:</strong> Kaff Apparels  
+        </p>
+        <p>10 days Return & Exchange</p>
+        <p><strong>Quantiy : </strong>5</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
