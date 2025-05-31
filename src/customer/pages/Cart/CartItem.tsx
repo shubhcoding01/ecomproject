7e23890e5e1b2e1788525078eef8918497b41b7d
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-31 14:04:04 +0530

    Implement CartItem component layout with image and styling

--- src/customer/pages/Cart/CartItem.tsx
+++ src/customer/pages/Cart/CartItem.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const CartItem = () => {
   return (
-    <div>CartItem</div>
+    <div className='border rounded-md relative'>
+        <div className='p-5 flex gap-3'>
+          <img className='w-[90px] rounded-md' src="https://m.media-amazon.com/images/I/61HS4sTDnPL._AC_AA152_.jpg" alt="" />
+        </div>
+    </div>
   )
 }
 
