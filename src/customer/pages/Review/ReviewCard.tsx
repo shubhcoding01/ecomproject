a704c2f209f4fe67b361980086efba98245ea1a4
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-31 10:32:28 +0530

    Refactor ReviewCard component to include Rating and Delete icon functionality

--- src/customer/pages/Review/ReviewCard.tsx
+++ src/customer/pages/Review/ReviewCard.tsx
@@ -1,4 +1,5 @@
-import { Avatar, Box, Grid, } from '@mui/material'
+import { Delete } from '@mui/icons-material'
+import { Avatar, Box, Grid, IconButton, Rating, } from '@mui/material'
 
 
 import React from 'react'
@@ -21,7 +22,22 @@ const ReviewCard = () => {
               <p className='opacity-70'>2025-05-31T10:06:18</p>
             </div>
           </div>
+          <Rating
+          readOnly
+          value={4.5}
+          precision={0.5}
+
+          />
+          <p>Value for money product, great product</p>
+          <div>
+            <img className='w-24 h-24 object-cover' src="https://m.media-amazon.com/images/I/81+6II6O3hL.jpg" alt="" />
+          </div>
         </Grid>
+
+          <IconButton>
+            <Delete/>
+          </IconButton>
+
         </Grid>
         
     </div>
