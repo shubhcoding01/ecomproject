b804f3731a0907ea2d8030a55da6240f90271542
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-31 10:25:24 +0530

    Updated ReviewCard component to use new props structure

--- src/customer/pages/Review/ReviewCard.tsx
+++ src/customer/pages/Review/ReviewCard.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 const ReviewCard = () => {
   return (
     <div className='flex justify-between'>
-        <Grid container spacing={2} sx={{ gap: 3 }}>
+        <Grid container spacing={8}  >
           <Grid  size={{xs:1}}>
             <Box >
               <Avatar className='text-white' sx={{width:56,height:56, bgcolor:"#1976d2"}}>
@@ -14,7 +14,16 @@ const ReviewCard = () => {
               </Avatar>
             </Box>
           </Grid>
+          <Grid size={{xs:9}}>
+          <div className='space-y-2'>
+            <div>
+              <p className='font-semibold text-lg'>Shubh</p>
+              <p className='opacity-70'>2025-05-31T10:06:18</p>
+            </div>
+          </div>
         </Grid>
+        </Grid>
+        
     </div>
   )
 }
