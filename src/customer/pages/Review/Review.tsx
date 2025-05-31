61f672a82e17bd27ebf5bac791b074ea9b119711
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-31 10:46:13 +0530

    Add Review component layout with product details and image

--- src/customer/pages/Review/Review.tsx
+++ src/customer/pages/Review/Review.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Review = () => {
   return (
-    <div>Review</div>
+    <div className='p-5 lg:px-20 flex flex-col lg:flex-row gap-20'>
+
+      <section className='w-full md:w-1/2 lg:w-[30%] space-y-2'>
+        <img src="https://images-eu.ssl-images-amazon.com/images/I/61HS4sTDnPL._AC_SR300,375,0,C_BR3_.jpg" alt="" />
+        <div>
+          <h1 className='text-lg font-semibold'>R Clothing</h1>
+          <p className='text-gray-600'>Stylish Men's Shirt</p>
+        </div>
+      </section>
+      
+    </div>
   )
 }
 
