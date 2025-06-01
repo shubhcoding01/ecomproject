7c42bd3728c3114000774a2c1967ced1222e2c84
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-01 15:59:19 +0530

    Add coupon code input and functionality to Cart component

--- src/customer/pages/Cart/Cart.tsx
+++ src/customer/pages/Cart/Cart.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CartItem from './CartItem'
+import { LocalOffer } from '@mui/icons-material'
+import { teal } from '@mui/material/colors'
+import { Button, TextField } from '@mui/material'
 
 const Cart = () => {
+  const [couponCode, setCouponCode] = useState("")
+  const handleChange = (e:any) => {
+    setCouponCode(e.target.value)
+  }
   return (
     <div className='pt-10 px-5 sm:px-10 md:px-60  min-h-screen'>
 
@@ -10,10 +17,25 @@ const Cart = () => {
             <div className='cartItemSection lg:col-span-2 space-y-3'>
                 {[1,1,1,1,1,1].map((item) => <CartItem/> )}
             </div>
-            <div className='col-span-1 text-sm space-y-3 border'>
-                <div>
+            <div className='col-span-1 text-sm space-y-3 '>
+
+                <div className='border rounded-md px-5 py-3 space-y-5'>
+                  
+                    <div className='flex gap-3 items-center text-sm'>
+                      <div className='flex gap-3 items-center text-sm'>
+                      <LocalOffer sx={{color:teal[600], fontSize:"17px"}}/>
+                    </div>
+                    <span>Apply Copons</span>
+                    </div>
+                    <div>
+                      <TextField onChange={handleChange} id="outlined-basic" placeholder='coupon code' size='small' variant="outlined" />
+                      <Button size='small'>
+                        Apply
+                      </Button>
+                    </div>
+                  </div>
                     
-                </div>
+                
             </div>
         </div>
     </div>
