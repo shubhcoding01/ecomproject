5ec08b21a8d4711ca0b7c136b134042e5b7694e5
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-01 23:47:26 +0530

    Fix label and validation for address field in Addressform component

--- src/customer/pages/checkout/Addressform.tsx
+++ src/customer/pages/checkout/Addressform.tsx
@@ -75,12 +75,12 @@ const Addressform = () => {
                     <Grid size={{xs:12}}>
                         <TextField
                             fullWidth
-                            label="Name"
-                            name='name'
-                            value={formik.values.name}
+                            label="address"
+                            name='address'
+                            value={formik.values.address}
                             onChange={formik.handleChange}
-                            error={formik.touched.name && Boolean(formik.errors.name)}
-                            helperText={formik.touched.name && formik.errors.name}
+                            error={formik.touched.address && Boolean(formik.errors.address)}
+                            helperText={formik.touched.name && formik.errors.address}
                             className="w-full p-2 border rounded-md"
                             required
                         />
