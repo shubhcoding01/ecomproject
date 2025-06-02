cc52ef3db7125b6c1e77ef505036724dfcb57148
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-02 14:31:52 +0530

    Add layout structure to Account component

--- src/customer/pages/Account/Account.tsx
+++ src/customer/pages/Account/Account.tsx
@@ -8,6 +8,11 @@ const Account = () => {
             <h1 className='text-xl font-bold pb-5'>Shubh</h1>
         </div>
         <Divider/>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 pt-5 lg:min-h-[78vh]'>
+            <section className='col-span-1 lg:border-r lg:pr-5 py-5 h-full'>
+                
+            </section>
+        </div>
     </div>
   )
 }
