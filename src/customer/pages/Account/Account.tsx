af03c00a87f3eb3b11d1a8dab0b75481922fc468
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-02 14:19:42 +0530

    Refactor Account component to include layout and user name display

--- src/customer/pages/Account/Account.tsx
+++ src/customer/pages/Account/Account.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Account = () => {
   return (
-    <div>Account</div>
+    <div className='pt-10 px-5 sm:px-10 md:px-60 min-h-screen'>
+        <div>
+            <h1 className='text-xl font-bold pb-5'>Shubh</h1>
+        </div>
+    </div>
   )
 }
 
