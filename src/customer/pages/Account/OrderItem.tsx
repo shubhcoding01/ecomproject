e2e055cfa3cc057f6c2fee8af600eee42b10eacb
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-02 17:16:11 +0530

    Implement OrderItem component layout to display order details with improved styling

--- src/customer/pages/Account/OrderItem.tsx
+++ src/customer/pages/Account/OrderItem.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
+import { teal } from '@mui/material/colors';
+import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
 
 const OrderItem = () => {
   return (
-    <div>OrderItem</div>
-  )
-}
+    <div className='text-sm bg-white p-5 space-y-4 border rounded-md cursor-pointer'>
+      <div className='flex items-center gap-5'>
+        <div>
+          <Avatar sx={{ bgcolor: teal[500] }}>
+            <ElectricBoltIcon />
+          </Avatar>
+        </div>
+        <div>
+          <h1 className="font-bold text-primary-color">PENDING</h1>
+          <p>Order details or description can go here.</p>
+        </div>
+      </div>
+      <div>
+        <p className='text-gray-500'>Order ID: 123456789</p>
+        <p className='text-gray-500'>Placed on: 2023-10-01</p>
+      </div>
+    </div>
+  );
+};
 
-export default OrderItem
+export default OrderItem;
