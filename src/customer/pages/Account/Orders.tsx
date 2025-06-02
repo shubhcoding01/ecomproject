93070a2e3f02ca73552791ec674a786940651d63
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-02 17:11:07 +0530

    Implement Orders component layout to display order details and improve user experience

--- src/customer/pages/Account/Orders.tsx
+++ src/customer/pages/Account/Orders.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import OrderItem from './OrderItem'
 
 const Orders = () => {
   return (
-    <div>Orders</div>
+    <div className='text-sm min-h-screen'>
+      <div className='pb-5'>
+        <h1 className='font-semibold'>All Orders</h1>
+        <p>from anytime</p>
+      </div>
+      <div className='space-y-2'>
+
+        {[1, 2, 3, 4, 5].map((item) => <OrderItem/>)}
+
+      </div>
+    </div>
   )
 }
 
