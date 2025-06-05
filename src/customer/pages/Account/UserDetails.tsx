73d038986699079df92fce0577a9a26a261e4e3f
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-05 15:56:33 +0530

    Add ProfileFieldCard component to UserDetails for displaying user name

--- src/customer/pages/Account/UserDetails.tsx
+++ src/customer/pages/Account/UserDetails.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ProfileFieldCard from '../../../component/ProfileFieldCard'
 
 const UserDetails = () => {
   return (
@@ -20,9 +21,9 @@ const UserDetails = () => {
         <div className='w-full lg:w-[70%]'>
             <div className='flex items-center pb-3 justify-between'>
                 <h1 className='text-2xl font-bold text-gray-700'>Personal Details</h1>
-                
-                
-
+            </div>
+            <div>
+                <ProfileFieldCard key="Name" value="John Doe" />
             </div>
 
         </div>
