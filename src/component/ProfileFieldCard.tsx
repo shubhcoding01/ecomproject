22846384b6686f888269e24843a3672ebf546a4c
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-05 15:52:58 +0530

    Add value display to ProfileFieldCard component

--- src/component/ProfileFieldCard.tsx
+++ src/component/ProfileFieldCard.tsx
@@ -6,6 +6,7 @@ const ProfileFieldCard = () => {
     <div className='p-5 flex items-center bg-slate-50'>
         <p className='w-20 lg:w-36 pr-5'>{key}</p>
         <Divider orientation="vertical" flexItem />
+        <p className='pl-5'>{value}</p>
     </div>
   )
 }
