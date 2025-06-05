59a55e3321e11756597f4c382d13bac2a2ac6e1a
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-05 15:51:39 +0530

    Add vertical divider to ProfileFieldCard component layout

--- src/component/ProfileFieldCard.tsx
+++ src/component/ProfileFieldCard.tsx
@@ -1,9 +1,11 @@
+import { Divider } from '@mui/material'
 import React from 'react'
 
 const ProfileFieldCard = () => {
   return (
     <div className='p-5 flex items-center bg-slate-50'>
         <p className='w-20 lg:w-36 pr-5'>{key}</p>
+        <Divider orientation="vertical" flexItem />
     </div>
   )
 }
