a6005a32975f3eaf9f709a95d4c9865fb97c6b26
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-05 15:45:08 +0530

    Add UserDetails component to display personal information on Account page

--- src/customer/pages/Account/UserDetails.tsx
+++ src/customer/pages/Account/UserDetails.tsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 const UserDetails = () => {
   return (
-    <div>UserDetails</div>
+    <div className='flex justify-center py-10'>
+        {/* {Personal Details} */}
+
+        <div className='w-full lg:w-[30%]'>
+            <div className='flex items-center pb-3 justify-between'>
+                <h1 className='text-xl font-bold'>Personal Details</h1>
+            </div>
+            <div className='space-y-3'>
+                <p className='text-gray-700'>Name: John Doe</p>
+                <p className='text-gray-700'>Email:</p>
+                <p className='text-gray-700'>Phone: +1234567890</p>
+                <p className='text-gray-700'>Address: 123 Main St, City, Country</p>
+            </div>
+
+        <div className='w-full lg:w-[70%]'>
+            <div className='flex items-center pb-3 justify-between'>
+                
+                
+
+            </div>
+
+        </div>
+
+    </div>
   )
 }
 
