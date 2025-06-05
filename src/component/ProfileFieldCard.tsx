a3bead8adfde9cd12208043bd24ba66f877774c2
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-05 22:28:28 +0530

    Fix ProfileFieldCard to use 'keys' prop instead of 'key' for consistency

--- src/component/ProfileFieldCard.tsx
+++ src/component/ProfileFieldCard.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const ProfileFieldCard = ({keys,value}:{keys:string,value:string}) => {
   return (
     <div className='p-5 flex items-center bg-slate-50'>
-        <p className='w-20 lg:w-36 pr-5'>{key}</p>
+        <p className='w-20 lg:w-36 pr-5'>{keys}</p>
         <Divider orientation="vertical" flexItem />
         <p className='pl-5'>{value}</p>
     </div>
