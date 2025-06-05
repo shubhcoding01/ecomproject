9bc55a2ae395dbc70dc9a2c5ac0a227c3818b9eb
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-05 15:51:10 +0530

    Add responsive styling to key display in ProfileFieldCard component

--- src/component/ProfileFieldCard.tsx
+++ src/component/ProfileFieldCard.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ProfileFieldCard = () => {
   return (
     <div className='p-5 flex items-center bg-slate-50'>
-        <p>{key}</p>
+        <p className='w-20 lg:w-36 pr-5'>{key}</p>
     </div>
   )
 }
