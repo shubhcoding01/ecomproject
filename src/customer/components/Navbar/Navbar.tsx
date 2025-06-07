e91f28800e2d3852a88e56a015bfc41b61e01c16
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-07 20:50:23 +0530

    Update Navbar component to display user avatar conditionally

--- src/customer/components/Navbar/Navbar.tsx
+++ src/customer/components/Navbar/Navbar.tsx
@@ -50,7 +50,7 @@ const Navbar = () => {
                         <SearchIcon />
                     </IconButton>
                     {
-                        false ? <Button className='flex items-center gap-2' >
+                        true ? <Button className='flex items-center gap-2' >
                             <Avatar
                             sx={{ width: 30, height: 30 }}
                             src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fmedium.com%2F%40simo_sultan%2Fa-rough-guide-to-create-a-css-avatar-bb9888011887&psig=AOvVaw1xRSffZbsw0GBV7xbUz2Lb&ust=1748364569229000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCID3iN7LwY0DFQAAAAAdAAAAABAE'/>
