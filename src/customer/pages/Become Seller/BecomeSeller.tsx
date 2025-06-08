9fdf8936b811b7072e39117dd91b56b6d9b4d81f
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-08 12:31:08 +0530

    Refactor BecomeSeller component layout for improved styling

--- src/customer/pages/Become Seller/BecomeSeller.tsx
+++ src/customer/pages/Become Seller/BecomeSeller.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const BecomeSeller = () => {
   return (
     <div className='grid md:gap-10 grid-cols-3 min-h-screen'>
-        <section>
-            
+        <section className='lg:col-span-1 md:col-span-2 col-span-3 p-10 shadow-lg rounded-b-md bg-white'>
+
         </section>
 
     </div>
