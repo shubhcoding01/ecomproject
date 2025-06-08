934dbbbb5c9a293e0b52c7a9ad2acabeccc99b24
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-08 13:29:22 +0530

    Add steps array to SellerAccountForm component

--- src/customer/pages/Become Seller/SellerAccountForm.tsx
+++ src/customer/pages/Become Seller/SellerAccountForm.tsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react'
 
+const steps = [
+    "Tax Details & Mobile",
+    "Pickup Address",
+    "Bank Details",
+    "Business Details",
+]
 const SellerAccountForm = () => {
     const [activeStep, setActiveStep] = useState(1);
-    
+
   return (
     <div>SellerAccountForm</div>
   )
