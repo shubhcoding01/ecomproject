e1205eed828d81356f34d336912a5c470c35f70a
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-08 12:29:13 +0530

    Refactor BecomeSeller component layout

--- src/customer/pages/Become Seller/BecomeSeller.tsx
+++ src/customer/pages/Become Seller/BecomeSeller.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const BecomeSeller = () => {
   return (
-    <div>BecomeSeller</div>
+    <div className='grid md:gap-10 grid-cols-3 min-h-screen'>
+        <section>
+            
+        </section>
+
+    </div>
   )
 }
 
