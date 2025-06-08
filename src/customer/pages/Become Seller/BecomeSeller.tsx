190ff0cba90f9211dd418e4b4dc43ae351240084
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-08 12:35:26 +0530

    Add handleShowPage function to toggle login state in BecomeSeller component

--- src/customer/pages/Become Seller/BecomeSeller.tsx
+++ src/customer/pages/Become Seller/BecomeSeller.tsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react'
 
 const BecomeSeller = () => {
-    const [isLogin, setIsLogin] = useState(false)
+    const [isLogin, setIsLogin] = useState(false);
+    
+    const handleShowPage = () => {
+        setIsLogin(!isLogin);
+    }
+
   return (
     <div className='grid md:gap-10 grid-cols-3 min-h-screen'>
         <section className='lg:col-span-1 md:col-span-2 col-span-3 p-10 shadow-lg rounded-b-md bg-white'>
