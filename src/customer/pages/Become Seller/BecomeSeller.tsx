6e354afe1ac85ce1ef2472fdd832265c6995cd82
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-08 12:32:22 +0530

    Add state management for login status in BecomeSeller component

--- src/customer/pages/Become Seller/BecomeSeller.tsx
+++ src/customer/pages/Become Seller/BecomeSeller.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const BecomeSeller = () => {
+    const [isLogin, setIsLogin] = useState(false)
   return (
     <div className='grid md:gap-10 grid-cols-3 min-h-screen'>
         <section className='lg:col-span-1 md:col-span-2 col-span-3 p-10 shadow-lg rounded-b-md bg-white'>
