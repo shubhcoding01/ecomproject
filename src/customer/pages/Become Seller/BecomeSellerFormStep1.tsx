77ff0afa1144234b3ee3db51f44d5ed7fe966d9b
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-08 14:20:22 +0530

    Add mobile TextField to BecomeSellerFormStep1 for contact details

--- src/customer/pages/Become Seller/BecomeSellerFormStep1.tsx
+++ src/customer/pages/Become Seller/BecomeSellerFormStep1.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import { Box, TextField } from '@mui/material'
 import React from 'react'
 
 const BecomeSellerFormStep1 = ({formik}:any) => {
@@ -7,6 +7,18 @@ const BecomeSellerFormStep1 = ({formik}:any) => {
         <p className='text-xl font-bold text-center pb-9'>Contact Details</p>
 
         <div className='space-y-9'>
+
+            <TextField
+                          fullWidth
+                          label="Mobile"
+                          name="mobile"
+                          value={formik.values.mobile}
+                          onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}
+                          error={formik.touched.mobile && Boolean(formik.errors.mobile)}
+                          helperText={formik.touched.mobile && formik.errors.mobile}
+                          required
+                        />
 
         </div>
     </Box>
