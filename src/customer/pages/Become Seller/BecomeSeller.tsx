2be118ef605e3b7a532b08d27fad068ef167759a
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-08 12:36:58 +0530

    Render SellerAccountForm or SellerLoginForm based on login state in BecomeSeller component

--- src/customer/pages/Become Seller/BecomeSeller.tsx
+++ src/customer/pages/Become Seller/BecomeSeller.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import SellerAccountForm from './SellerAccountForm';
+import SellerLoginForm from './SellerLoginForm';
 
 const BecomeSeller = () => {
     const [isLogin, setIsLogin] = useState(false);
@@ -11,6 +13,7 @@ const BecomeSeller = () => {
     <div className='grid md:gap-10 grid-cols-3 min-h-screen'>
         <section className='lg:col-span-1 md:col-span-2 col-span-3 p-10 shadow-lg rounded-b-md bg-white'>
 
+            {isLogin ? <SellerAccountForm /> : <SellerLoginForm />}
         </section>
 
     </div>
