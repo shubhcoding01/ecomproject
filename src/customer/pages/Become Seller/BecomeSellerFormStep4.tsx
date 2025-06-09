73850680941aea688d491f79fdff6681bd674a53
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-09 11:55:07 +0530

    Implement BecomeSellerFormStep4 component with formik integration and input fields for business name, seller name, and business email

--- src/customer/pages/Become Seller/BecomeSellerFormStep4.tsx
+++ src/customer/pages/Become Seller/BecomeSellerFormStep4.tsx
@@ -1,12 +1,51 @@
+import { TextField } from '@mui/material';
 import React from 'react'
 
 interface BecomeSellerFormStep2Props {
     formik: any;
 }
 
-const BecomeSellerFormStep4 =() => {
+const BecomeSellerFormStep4 =({formik}:BecomeSellerFormStep2Props) => {
   return (
-    <div>BecomeSellerFormStep4</div>
+    <div className='space-y-9'>
+
+        <TextField
+            fullWidth
+            label="Business Name"
+            name="businessDetails.businessName"
+            value={formik.values.businessDetails.businessName}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.businessDetails?.businessName && Boolean(formik.errors.businessDetails?.businessName)}
+            helperText={formik.touched.businessDetails?.businessName && formik.errors.businessDetails?.businessName}
+            required
+        />
+
+        <TextField
+            fullWidth
+            name='sellerName'
+            label="Seller Name"
+            value={formik.values.sellerName}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.sellerName && Boolean(formik.errors.sellerName)}
+            helperText={formik.touched.sellerName && formik.errors.sellerName}
+            required
+        />
+        
+        <TextField
+            fullWidth
+            label="Business Email"
+            name="businessDetails.businessEmail"
+            value={formik.values.businessDetails.businessEmail}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.businessDetails?.businessEmail && Boolean(formik.errors.businessDetails?.businessEmail)}
+            helperText={formik.touched.businessDetails?.businessEmail && formik.errors.businessDetails?.businessEmail}
+            required
+        />
+
+    </div>
   )
 }
 
