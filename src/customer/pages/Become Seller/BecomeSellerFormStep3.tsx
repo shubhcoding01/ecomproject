902afdc75aa7ef5cbe909c0b03613ac2c73b61cf
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-09 11:46:38 +0530

    Add TextField for account number input in BecomeSellerFormStep3 component

--- src/customer/pages/Become Seller/BecomeSellerFormStep3.tsx
+++ src/customer/pages/Become Seller/BecomeSellerFormStep3.tsx
@@ -1,3 +1,4 @@
+import { TextField } from '@mui/material';
 import React from 'react'
 
 interface BecomeSellerFormStep2Props {
@@ -7,6 +8,18 @@ interface BecomeSellerFormStep2Props {
 const BecomeSellerFormStep3:React.FC<BecomeSellerFormStep2Props> = ({formik}) => {
   return (
     <div className='space-y-9'>
+
+        <TextField
+          fullWidth
+          label="Account Number"
+          name="bankDetails.accountNumber"
+          value={formik.values.bankDetails.accountNumber}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.bankDetails?.accountNumber && Boolean(formik.errors.bankDetails?.accountNumber)}
+          helperText={formik.touched.bankDetails?.accountNumber && formik.errors.bankDetails?.accountNumber}
+          required
+          />
 
     </div>
   )
