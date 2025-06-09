b6b9707073c6df61d73ebef1bb6d0c9a5eb4ae0d
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-09 12:05:30 +0530

    Implement SellerLoginForm with email input field and formik integration

--- src/customer/pages/Become Seller/SellerLoginForm.tsx
+++ src/customer/pages/Become Seller/SellerLoginForm.tsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const SellerLoginForm = () => {
   return (
-    <div>SellerLoginForm</div>
+    <div>
+      <div className='spyace-y-9'>
+        <TextField
+          fullWidth
+          label="Email"
+          name="email"
+          type="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+          required
+        />
+
+      </div>
+    </div>
   )
 }
 
