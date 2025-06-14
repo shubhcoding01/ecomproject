3c6f907ba0d54aecbf0f4073e0c52d45d35f3a32
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-14 21:48:16 +0530

    Add "Add Product" menu item to SellerDrawerList component

--- src/seller/components/SellerDrawerList/SellerDrawerList.tsx
+++ src/seller/components/SellerDrawerList/SellerDrawerList.tsx
@@ -1,4 +1,4 @@
-import { Dashboard, Inventory, ShoppingBag } from '@mui/icons-material'
+import { Add, Dashboard, Inventory, ShoppingBag } from '@mui/icons-material'
 import React from 'react'
 
 const menu = [
@@ -19,6 +19,12 @@ const menu = [
     path:"/seller/products",
     icon:<Inventory className='text-primary-color'/>,
     activeIcon:<Inventory className='text-white'/>
+  },
+  {
+    name:"Add Product",
+    path:"/seller/add-product",
+    icon:<Add className='text-primary-color'/>,
+    activeIcon:<Add className='text-white'/>
   },
 
 ]
