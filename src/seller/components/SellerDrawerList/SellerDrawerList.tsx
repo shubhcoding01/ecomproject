1e0f320ef1a967bd9e11cc174c549e23da0a652c
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-14 21:39:57 +0530

    Add menu structure to SellerDrawerList component

--- src/seller/components/SellerDrawerList/SellerDrawerList.tsx
+++ src/seller/components/SellerDrawerList/SellerDrawerList.tsx
@@ -1,4 +1,14 @@
+import { Dashboard } from '@mui/icons-material'
 import React from 'react'
+
+const menu = [
+  {
+    name:"Dashboard",
+    path:"/seller",
+    icon:<Dashboard className='text-primary-color'/>,
+    activeIcon:<Dashboard className='text-white'/>
+  }
+]
 
 const SellerDrawerList = () => {
   return (
