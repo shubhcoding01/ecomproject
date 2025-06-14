0340e850796776c83b37b810462c331dc69fda16
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-14 21:35:31 +0530

    Add structure to DrawerList component

--- src/component/DrawerList.tsx
+++ src/component/DrawerList.tsx
@@ -4,7 +4,11 @@ const DrawerList = () => {
   return (
     <div className='h-full'>
       <div className='flex flex-col justify-between h-full w-[300px] border-r py-5'>
+          <div>
+            <div className='space-y-2'>
 
+            </div>
+          </div>
       </div>
     </div>
   )
