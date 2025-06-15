13811d2ad8aa1aeb897e7dfe0813fdcb2d623c46
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-15 20:19:00 +0530

    Refactor DrawerList, SellerDrawerList, and SellerDashboard components for improved readability and structure

--- src/component/DrawerList.tsx
+++ src/component/DrawerList.tsx
@@ -1,88 +1,88 @@
-// import { ListItemIcon, ListItemText } from '@mui/material'
-// import React from 'react'
+import { ListItemIcon, ListItemText } from '@mui/material'
+import React from 'react'
 
-// interface menuItem{
+interface menuItem{
   
-//       name:string,
-//       path:string,
-//       icon:any
-//       activeIcon:any
+      name:string,
+      path:string,
+      icon:any
+      activeIcon:any
     
-// }
-
-// interface DrawerListProps {
-//   menu:menuItem[],
-//   menu2:menuItem[],
-//   toggleDrawer:()=>void
-// }
-
-// const DrawerList = ({menu,menu2,toggleDrawer}:DrawerListProps) => {
-//   return (
-//     <div className='h-full'>
-//       <div className='flex flex-col justify-between h-full w-[300px] border-r py-5'>
-//           <div>
-//             <div className='space-y-2'>
-//                 {
-//                   menu.map((item:any,index:number)=>
-//                     <div className='pr-9' key={index}>
-//                       <p>
-//                         <ListItemIcon>
-//                           {item.icon}
-//                         </ListItemIcon>
-//                         <ListItemText primary={item.name}/>
-//                       </p>
-//                     </div>
-//                   )
-//                 }
-//             </div>
-//           </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default DrawerList
-
-// DrawerList.tsx
-import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
-import React from 'react';
-
-interface MenuItem {
-  name: string;
-  path: string;
-  icon: React.ReactNode;
-  activeIcon: React.ReactNode;
 }
 
 interface DrawerListProps {
-  menu: MenuItem[];
-  menu2: MenuItem[];
-  toggleDrawer: () => void;
+  menu:menuItem[],
+  menu2:menuItem[],
+  toggleDrawer:()=>void
 }
 
-const DrawerList: React.FC<DrawerListProps> = ({ menu, menu2, toggleDrawer }) => {
+const DrawerList = ({menu,menu2,toggleDrawer}:DrawerListProps) => {
   return (
     <div className='h-full'>
       <div className='flex flex-col justify-between h-full w-[300px] border-r py-5'>
-        <List>
-          {menu.map((item, index) => (
-            <ListItem key={index} onClick={toggleDrawer} style={{ cursor: 'pointer' }}>
-              <ListItemIcon>{item.icon}</ListItemIcon>
-              <ListItemText primary={item.name} />
-            </ListItem>
-          ))}
-        </List>
-        <List>
-          {menu2.map((item, index) => (
-            <ListItem key={index} onClick={toggleDrawer} style={{ cursor: 'pointer' }}>
-              <ListItemIcon>{item.icon}</ListItemIcon>
-              <ListItemText primary={item.name} />
-            </ListItem>
-          ))}
-        </List>
+          <div>
+            <div className='space-y-2'>
+                {
+                  menu.map((item:any,index:number)=>
+                    <div className='pr-9' key={index}>
+                      <p>
+                        <ListItemIcon>
+                          {item.icon}
+                        </ListItemIcon>
+                        <ListItemText primary={item.name}/>
+                      </p>
+                    </div>
+                  )
+                }
+            </div>
+          </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default DrawerList;
+export default DrawerList
+
+// DrawerList.tsx
+// import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+// import React from 'react';
+
+// interface MenuItem {
+//   name: string;
+//   path: string;
+//   icon: React.ReactNode;
+//   activeIcon: React.ReactNode;
+// }
+
+// interface DrawerListProps {
+//   menu: MenuItem[];
+//   menu2: MenuItem[];
+//   toggleDrawer: () => void;
+// }
+
+// const DrawerList: React.FC<DrawerListProps> = ({ menu, menu2, toggleDrawer }) => {
+//   return (
+//     <div className='h-full'>
+//       <div className='flex flex-col justify-between h-full w-[300px] border-r py-5'>
+//         <List>
+//           {menu.map((item, index) => (
+//             <ListItem key={index} onClick={toggleDrawer} style={{ cursor: 'pointer' }}>
+//               <ListItemIcon>{item.icon}</ListItemIcon>
+//               <ListItemText primary={item.name} />
+//             </ListItem>
+//           ))}
+//         </List>
+//         <List>
+//           {menu2.map((item, index) => (
+//             <ListItem key={index} onClick={toggleDrawer} style={{ cursor: 'pointer' }}>
+//               <ListItemIcon>{item.icon}</ListItemIcon>
+//               <ListItemText primary={item.name} />
+//             </ListItem>
+//           ))}
+//         </List>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default DrawerList;
--- src/seller/pages/SellerDashboard/SellerDashboard.tsx
+++ src/seller/pages/SellerDashboard/SellerDashboard.tsx
@@ -1,49 +1,49 @@
-// import React from 'react'
-// import SellerDrawerList from '../../components/SellerDrawerList/SellerDrawerList'
+import React from 'react'
+import SellerDrawerList from '../../components/SellerDrawerList/SellerDrawerList'
 
-// const SellerDashboard = () => {
-//   const toggleDrawer = () => {}
-//   return (
-//     <div>
-//         <div className='lg:flex lg:h-[90vh]'>
-
-//             <section className="hidden lg:block h-full">
-//                 <SellerDrawerList toggleDrawer={toggleDrawer}/>
-//             </section>
-//             <section className="p-10 w-full lg:w-[80%] overflow-y-auto">
-//                 Seller Routes
-//             </section>
-
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default SellerDashboard
-
-// SellerDashboard.tsx
-import React from 'react';
-import SellerDrawerList from '../../components/SellerDrawerList/SellerDrawerList';
-
-const SellerDashboard: React.FC = () => {
-  const toggleDrawer = () => {
-    // Implement drawer toggle functionality
-  };
-
+const SellerDashboard = () => {
+  const toggleDrawer = () => {}
   return (
     <div>
-      <div className='lg:flex lg:h-[90vh]'>
-        <section className="hidden lg:block h-full">
-          <SellerDrawerList toggleDrawer={toggleDrawer} />
-        </section>
-        <section className="p-10 w-full lg:w-[80%] overflow-y-auto">
-          <h1 className="text-2xl font-bold">Seller Dashboard</h1>
-          <p>Manage your seller account and view your sales data here.</p>
-          {/* Add more content as needed */}
-        </section>
-      </div>
-    </div>
-  );
-};
+        <div className='lg:flex lg:h-[90vh]'>
 
-export default SellerDashboard;
+            <section className="hidden lg:block h-full">
+                <SellerDrawerList toggleDrawer={toggleDrawer}/>
+            </section>
+            <section className="p-10 w-full lg:w-[80%] overflow-y-auto">
+                Seller Routes
+            </section>
+
+        </div>
+    </div>
+  )
+}
+
+export default SellerDashboard
+
+// SellerDashboard.tsx
+// import React from 'react';
+// import SellerDrawerList from '../../components/SellerDrawerList/SellerDrawerList';
+
+// const SellerDashboard: React.FC = () => {
+//   const toggleDrawer = () => {
+//     // Implement drawer toggle functionality
+//   };
+
+//   return (
+//     <div>
+//       <div className='lg:flex lg:h-[90vh]'>
+//         <section className="hidden lg:block h-full">
+//           <SellerDrawerList toggleDrawer={toggleDrawer} />
+//         </section>
+//         <section className="p-10 w-full lg:w-[80%] overflow-y-auto">
+//           <h1 className="text-2xl font-bold">Seller Dashboard</h1>
+//           <p>Manage your seller account and view your sales data here.</p>
+//           {/* Add more content as needed */}
+//         </section>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SellerDashboard;
