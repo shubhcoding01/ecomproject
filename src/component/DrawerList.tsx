3843b111c54a449db57c88c308369c03f4061fc5
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-15 20:34:22 +0530

    Enhance DrawerList component styling for active menu items with improved layout and padding

--- src/component/DrawerList.tsx
+++ src/component/DrawerList.tsx
@@ -27,7 +27,7 @@ const DrawerList = ({menu,menu2,toggleDrawer}:DrawerListProps) => {
                 {
                   menu.map((item:any,index:number)=>
                     <div  className='pr-9 cursor-pointer' key={index}>
-                      <p className={`${item.path==location.pathname ? "bg-primary-color text-white":"text-primary-color"}`}>
+                      <p className={`${item.path==location.pathname ? "bg-primary-color text-white":"text-primary-color"} flex items-center px-5 py-3 rounded-r-full`}>
                         <ListItemIcon>
                           {item.icon==location.pathname?item.activeIcon:item.icon}
                         </ListItemIcon>
