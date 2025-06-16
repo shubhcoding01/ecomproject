db4f8b7a76a3bb67a7af96fad673e3d976d6d8e7
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-16 19:08:22 +0530

    Fix SellerRoutes to replace Product component with Products component

--- src/Routes/SellerRoutes.tsx
+++ src/Routes/SellerRoutes.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Dashboard from '../seller/pages/SellerDashboard/Dashboard'
-import Product from '../customer/pages/Product/Product'
 import AddProduct from '../seller/pages/Products/AddProduct'
 import Orders from '../seller/pages/Order/Orders'
 import Profile from '../seller/pages/Account/Profile'
 import Payment from '../seller/pages/Payment/Payment'
 import Transaction from '../seller/pages/Payment/Transaction'
+import Products from '../seller/pages/Products/Products'
 
 const SellerRoutes = () => {
   return (
     <div>
         <Routes>
             <Route path='/' element={<Dashboard/>}/>
-            <Route path='/product' element={<Product/>}/>
+            <Route path='/product' element={<Products/>}/>
             <Route path='/add-product' element={<AddProduct/>}/>
             <Route path='/orders' element={<Orders/>}/>
             <Route path='/account' element={<Profile/>}/>
