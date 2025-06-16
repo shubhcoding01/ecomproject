d1034d0168529c98827bb72b1faf5182f33e76cc
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-16 18:56:46 +0530

    Fix SellerRoutes to render Dashboard component

--- src/Routes/SellerRoutes.tsx
+++ src/Routes/SellerRoutes.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Dashboard from '../seller/pages/SellerDashboard/Dashboard'
 
 const SellerRoutes = () => {
   return (
     <div>
         <Routes>
-            <Route path='/*' element/>
+            <Route path='/*' element={<Dashboard/>}/>
         </Routes>
     </div>
   )
