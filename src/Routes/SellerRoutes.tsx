97bd37010c1b865caec7979a3a2f62221a92cd9b
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-16 19:02:48 +0530

    Fix SellerRoutes to correctly route to Product component and add Profile component file

--- src/Routes/SellerRoutes.tsx
+++ src/Routes/SellerRoutes.tsx
@@ -2,13 +2,20 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Dashboard from '../seller/pages/SellerDashboard/Dashboard'
 import Product from '../customer/pages/Product/Product'
+import AddProduct from '../seller/pages/Products/AddProduct'
+import Orders from '../seller/pages/Order/Orders'
 
 const SellerRoutes = () => {
   return (
     <div>
         <Routes>
             <Route path='/' element={<Dashboard/>}/>
-            <Route path='/' element={<Product/>}/>
+            <Route path='/product' element={<Product/>}/>
+            <Route path='/add-product' element={<AddProduct/>}/>
+            <Route path='/orders' element={<Orders/>}/>
+            <Route path='/account' element={<Profile/>}/>
+
+
         </Routes>
     </div>
   )
