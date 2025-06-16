5427c36fe730ecdea6a46af31136bc9adcad3a31
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-16 19:04:26 +0530

    Add Payment and Transaction routes to SellerRoutes

--- src/Routes/SellerRoutes.tsx
+++ src/Routes/SellerRoutes.tsx
@@ -4,6 +4,9 @@ import Dashboard from '../seller/pages/SellerDashboard/Dashboard'
 import Product from '../customer/pages/Product/Product'
 import AddProduct from '../seller/pages/Products/AddProduct'
 import Orders from '../seller/pages/Order/Orders'
+import Profile from '../seller/pages/Account/Profile'
+import Payment from '../seller/pages/Payment/Payment'
+import Transaction from '../seller/pages/Payment/Transaction'
 
 const SellerRoutes = () => {
   return (
@@ -14,7 +17,8 @@ const SellerRoutes = () => {
             <Route path='/add-product' element={<AddProduct/>}/>
             <Route path='/orders' element={<Orders/>}/>
             <Route path='/account' element={<Profile/>}/>
-
+            <Route path='/payment' element={<Payment/>}/>
+            <Route path='/transaction' element={<Transaction/>}/>
 
         </Routes>
     </div>
