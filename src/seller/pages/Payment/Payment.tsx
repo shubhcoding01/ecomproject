15e7dec2632f7684b0b6a2a12bbd5d0eeeb185eb
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-21 06:58:17 +0530

    Update Payment component to handle payment method selection

--- src/seller/pages/Payment/Payment.tsx
+++ src/seller/pages/Payment/Payment.tsx
@@ -1,8 +1,16 @@
+import { Card, Divider } from '@mui/material'
 import React from 'react'
 
 const Payment = () => {
   return (
-    <div>Payment</div>
+    <div className=''>
+      <Card className='rounded-md space-y-4 p-5'>
+        <h1 className='text-gray-600 font-medium'>Total Earning</h1>
+        <h1 className='font-bold text-xl pb-1'>₹11664</h1>
+        <Divider/>
+        <p className='text-gray-600 font-medium pt-1'>Last Payment : <strong>₹0</strong></p>
+      </Card>
+    </div>
   )
 }
 
