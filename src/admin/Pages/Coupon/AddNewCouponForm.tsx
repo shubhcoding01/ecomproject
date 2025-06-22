0a6e42cc25dd36522d24fff844b4eca20c66e51c
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-22 12:09:58 +0530

    Update the AddNewCouponForm to use the new coupon form component

--- src/admin/Pages/Coupon/AddNewCouponForm.tsx
+++ src/admin/Pages/Coupon/AddNewCouponForm.tsx
@@ -1,6 +1,9 @@
+import { LocalizationProvider } from '@mui/x-date-pickers'
 import { Dayjs } from 'dayjs'
 import { useFormik } from 'formik'
 import React from 'react'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { Box, Grid, TextField } from '@mui/material';
 
 interface CouponFormValues {
   code: string,
@@ -33,7 +36,49 @@ const AddNewCouponForm = () => {
   })
 
   return (
-    <div>AddNewCouponForm</div>
+    <div>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+
+        <Box component={"form"} onSubmit={formik.handleSubmit} sx={{ maxWidth: 600, margin: 'auto', padding: 2 }}>
+
+          <Grid container spacing={2}>
+            <Grid size={{xs:12,sm:6}}>
+
+              <TextField
+              fullWidth
+              label="coupon code"
+              name="code"
+              value={formik.values.code}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.code && Boolean(formik.errors.code)}
+              helperText={formik.touched.code && formik.errors.code}
+              required
+            />
+
+            </Grid>
+
+            <Grid size={{xs:12,sm:6}}>
+
+              <TextField
+              fullWidth
+              label="Discount Percentage"
+              name="discountPercentage"
+              value={formik.values.discountPercentage}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.discountPercentage && Boolean(formik.errors.discountPercentage)}
+              helperText={formik.touched.discountPercentage && formik.errors.discountPercentage}
+              required
+            />
+
+            </Grid>
+          </Grid>
+
+        </Box>
+
+      </LocalizationProvider>
+    </div>
   )
 }
 
