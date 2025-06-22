5bbd8e5f8ad46b780bbcba5ae1d5114be3532967
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-22 12:49:07 +0530

    Updated the Deal component to use the new `useDeal` hook

--- src/admin/Pages/HomePage/Deal.tsx
+++ src/admin/Pages/HomePage/Deal.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { Button } from '@mui/material'
+import React, { useState } from 'react'
+
+const tabs = [
+  "Deals",
+  "Category",
+  "Create Deal",
+]
 
 const Deal = () => {
+  const [activeTab, setActiveTab] = useState("Deals")
   return (
-    <div>Deal</div>
+    <div>
+      <div className='flex gap-4'>
+        {tabs.map((item) => <Button variant={activeTab==item?"contained":"outlined"}>{item}</Button>)}
+      </div>
+      <div className=''></div>
+    </div>
   )
 }
 
