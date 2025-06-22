9fdb7ba144522a7acffc533df8d94a1a029086dc
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-22 11:46:21 +0530

    Update package-lock.json and package.json to include new dependencies

--- src/admin/Pages/Coupon/AddNewCouponForm.tsx
+++ src/admin/Pages/Coupon/AddNewCouponForm.tsx
@@ -1,6 +1,22 @@
+import { useFormik } from 'formik'
 import React from 'react'
 
 const AddNewCouponForm = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      code: '',
+      discountPercentage: 0,
+      validityStartDate: null,
+      validityEndDate: null,
+      minimumOrderValue: 0
+    },
+    onSubmit: values => {
+      console.log('Form values:', values)
+      // Handle form submission logic here
+    }
+  })
+
   return (
     <div>AddNewCouponForm</div>
   )
