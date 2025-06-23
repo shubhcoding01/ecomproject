48820e523ee967936e03427a07435cd8aeddb327
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-06-23 08:43:12 +0530

    Update sellerProductSlice.ts to handle product deletion

--- src/State/seller/sellerProductSlice.ts
+++ src/State/seller/sellerProductSlice.ts
@@ -1,6 +1,7 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { api } from "../../config/Api";
 import { Product } from "../../types/ProductTypes";
+import { create } from "domain";
 
 export const fetchSellerProducts = createAsyncThunk<Product[],any>(
   "/sellerProduct/fetchSellerProducts",
@@ -37,3 +38,47 @@ export const createProduct = createAsyncThunk<Product,
         }
     }
  )
+
+ interface SellerProductState {
+    products: Product[];
+    loading: boolean;
+    error: string | null;
+}
+
+const initialState: SellerProductState = {
+    products: [],
+    loading: false,
+    error: null,
+}
+const sellerProductSlice = createSlice({
+    name: "sellerProduct",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchSellerProducts.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            builder.addCase(fetchSellerProducts.fulfilled, (state, action) => {
+                state.loading = false;
+                state.products = action.payload;
+            })
+            builder.addCase(fetchSellerProducts.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message || "Failed to fetch products";
+            })
+            builder.addCase(createProduct.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            builder.addCase(createProduct.fulfilled, (state, action) => {
+                state.loading = false;
+                state.products.push(action.payload);
+            })
+            builder.addCase(createProduct.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message || "Failed to create product";
+            });
+    },
+    });
