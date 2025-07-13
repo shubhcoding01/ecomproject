73e6ad1e3f138463e7509e6dbfd3c20af1e38215
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-07-13 20:38:15 +0530

    Updated commit message for recent changes

--- src/customer/pages/Auth/RegisterForm.tsx
+++ src/customer/pages/Auth/RegisterForm.tsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import { useAppDispatch } from '../../../State/Store';
+import { useFormik } from 'formik';
+import { sendLoginSignupOtp } from '../../../State/AuthSlice';
 
 const RegisterForm = () => {
+  const dispatch  = useAppDispatch();
+      const formik = useFormik({
+        initialValues: {
+          email: '',
+          otp: '',
+        },
+        onSubmit: (values) => {
+          console.log("Login values:", values);
+  
+        },
+      });
+  
+       const handleSendOtp = () =>{
+          dispatch(sendLoginSignupOtp({email:formik.values.email}))
+        }
   return (
     <div>
                 <h1 className='text-center font-bold text-xl text-primary-color pb-8'>Signup</h1>
