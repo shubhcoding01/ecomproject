f89ca576cf9dac88bf33469bc8e86471007fd187
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-07-13 19:19:03 +0530

    Update the commit message to reflect the changes made in the files.      <form onSubmit={formik.handleSubmit}>

--- src/customer/pages/Auth/LoginForm.tsx
+++ src/customer/pages/Auth/LoginForm.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { useAppDispatch } from '../../../State/Store';
+import { useFormik } from 'formik';
 
 const LoginForm = () => {
+  const dispatch  = useAppDispatch();
+    const formik = useFormik({
+      initialValues: {
+        email: '',
+        otp: '',
+      },
+      onSubmit: (values) => {
+        console.log("Login values:", values);
+
+      },
+    });
   return (
     <div>
         <h1 className='text-center font-bold text-xl text-primary-color pb-8'>Login</h1>
